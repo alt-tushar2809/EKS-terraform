7915eef0ba790409175ec8eb0a16b698680794ba
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-03-23 16:05:36 +0530

    --- Auto-Git Commit ---

--- external-ip.tf
+++ external-ip.tf
@@ -1,57 +1,8 @@
-variable "region" {
-  default     = "ap-southeast-2"
-  description = "AWS region"
+data "http" "workstation-external-ip" {
+  url = "http://ipv4.icanhazip.com"
 }
 
-variable "cluster_name" {
-  default = "getting-started-eks"
+locals {
+  workstation-external-cidr = "${chomp(data.http.workstation-external-ip.body)}/32"
 }
 
-variable "map_accounts" {
-  description = "Additional AWS account numbers to add to the aws-auth configmap."
-  type        = list(string)
-
-  default = [
-    "777777777777",
-    "888888888888",
-  ]
-}
-
-variable "map_roles" {
-  description = "Additional IAM roles to add to the aws-auth configmap."
-  type = list(object({
-    rolearn  = string
-    username = string
-    groups   = list(string)
-  }))
-
-  default = [
-    {
-      rolearn  = "arn:aws:iam::66666666666:role/role1"
-      username = "role1"
-      groups   = ["system:masters"]
-    },
-  ]
-}
-
-variable "map_users" {
-  description = "Additional IAM users to add to the aws-auth configmap."
-  type = list(object({
-    userarn  = string
-    username = string
-    groups   = list(string)
-  }))
-
-  default = [
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user1"
-      username = "user1"
-      groups   = ["system:masters"]
-    },
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user2"
-      username = "user2"
-      groups   = ["system:masters"]
-    },
-  ]
-}
